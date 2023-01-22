553dabdf683532eabab06d4730eab080f2257b5d
Khue Lee <khuelee55@gmail.com>
2023-01-22 13:58:43 -0600

    added GalleryList and GalleryItem compenents, added GET request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  useEffect( () => {
+    fetchGallery();
+  }, []);
+
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then((response) => {
+      console.log('Response from GET:', response);
+      // setGalleryList(response.data)
+    })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
+        
         <img src="images/goat_small.jpg"/>
       </div>
     );
